3ab4789dc4800948f6e8123f86d67582e3e33a69
MiltonR12 <miltonaguilar142018@gmail.co>
2025-06-10 01:05:04 -0400

    agregar mi db

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-export const MONGO_URI = 'mongodb://localhost:27017/alex_db';
+export const MONGO_URI = 'mongodb+srv://miltonxcvb12:miltonxcvb12@alex_db.vq6poup.mongodb.net/alex_db?retryWrites=true&w=majority&appName=alex_db';
+// export const MONGO_URI = 'mongodb://localhost:27017/alex_db';
 export const PORT = 4000;
 export const JWT_SECRET = "mi_screto_milton"
